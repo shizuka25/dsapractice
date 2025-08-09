06598a2dad60dffcb5cfad0494381cfcfe11ea6b
Aditii <hypercrazy511@gmail.com>
2025-08-09 19:14:33 +0530

    Time: 1 ms (35.75%), Space: 40.6 MB (91.35%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,5 +1,21 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        return n > 0 && (n & (n - 1)) == 0;
+        int ans =1;
+        for (int i =0; i<=30; i++){
+            if(ans==n){
+                return true;
+            }
+            if(ans * 2 < (Integer.MAX_VALUE)){
+            ans = ans*2;
+            }
+        }
+        return false;
     }
 }
+
+
+// class Solution {
+//     public boolean isPowerOfTwo(int n) {
+//         return n > 0 && (n & (n - 1)) == 0;
+//     }
+// }
