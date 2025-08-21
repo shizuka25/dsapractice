86a8844c5a85f91e5ebca4c1d3b661939dc71af7
Aditii <hypercrazy511@gmail.com>
2025-08-21 18:34:49 +0530

    Time: 2 ms (99.79%), Space: 43.3 MB (91.24%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,27 +1,81 @@
+// class Solution {
+//     public String simplifyPath(String path) {
+//         String[] components = path.split("/");
+//         Stack<String> st = new Stack<>();
+
+//         for (String comp : components) {
+//             if (comp.equals("") || comp.equals(".")) {
+//                 continue;
+//             }
+
+//             if (comp.equals("..")) {
+//                 if (!st.isEmpty()) {
+//                     st.pop();
+//                 }
+//             } else {
+//                 st.push(comp);
+//             }
+//         }
+
+//         StringBuilder sb = new StringBuilder();
+//         while (!st.isEmpty()) {
+//             sb.insert(0, "/" + st.pop());
+//         }
+
+//         return sb.length() == 0 ? "/" : sb.toString();        
+//     }
+// }
+
+
 class Solution {
     public String simplifyPath(String path) {
-        String[] components = path.split("/");
-        Stack<String> st = new Stack<>();
+        Stack<String> folders = new Stack<String>();
 
-        for (String comp : components) {
-            if (comp.equals("") || comp.equals(".")) {
+        int i = 0;
+        while (i < path.length()) {
+            while (i < path.length() && path.charAt(i) == '/') {
+                i++;
+            }
+
+            if (i == path.length()) {
                 continue;
             }
 
-            if (comp.equals("..")) {
-                if (!st.isEmpty()) {
-                    st.pop();
+            if (i < (path.length() - 1) && path.charAt(i) == '.' && path.charAt(i+1) == '.') {
+                if (i + 2 == path.length() || path.charAt(i + 2) == '/') {
+                    if (folders.size() != 0) {
+                        folders.pop();
+                    }
+                    i += 2;
+                    continue;
+                    
                 }
-            } else {
-                st.push(comp);
             }
+
+            if (path.charAt(i) == '.' && (i + 1 == path.length() || path.charAt(i + 1) == '/')) {
+                i += 1;
+                continue;
+            }
+
+            int j = i;
+            while (j < path.length() && path.charAt(j) != '/') {
+                j++;
+            }
+            String folder = path.substring(i, j);
+            i = j;
+            folders.push(folder);
+        }
+
+        if (folders.size() == 0) {
+            return "/";
         }
 
         StringBuilder sb = new StringBuilder();
-        while (!st.isEmpty()) {
-            sb.insert(0, "/" + st.pop());
+        for (String folder : folders) {
+            sb.append("/");
+            sb.append(folder);
         }
 
-        return sb.length() == 0 ? "/" : sb.toString();        
+        return sb.toString();
     }
 }
